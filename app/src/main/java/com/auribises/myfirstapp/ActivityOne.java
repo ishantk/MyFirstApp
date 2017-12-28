adb0473df53d78759ef6f4d4e9358a225b4acdf7
Ishant Kumar <er.ishant@gmail.com>
2017-12-28 15:13:31 +0530

    LifeCycle and Views

--- app/src/main/java/com/auribises/myfirstapp/ActivityOne.java
+++ app/src/main/java/com/auribises/myfirstapp/ActivityOne.java
@@ -3,21 +3,42 @@ package com.auribises.myfirstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 // Activity Life Cycle
 
 public class ActivityOne extends AppCompatActivity {
 
+    EditText eTxtA, eTxtB;
+    TextView txtTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
         Toast.makeText(this,"ActivityOne onCreate",Toast.LENGTH_LONG).show();
+
+        eTxtA = (EditText)findViewById(R.id.editText);
+        eTxtB = (EditText)findViewById(R.id.editText2);
+        txtTitle = (TextView)findViewById(R.id.textView);
+
     }
 
     public void clickHandler(View view){
-        Toast.makeText(this,"You Clicked Button",Toast.LENGTH_LONG).show();
+
+        String num1 = eTxtA.getText().toString().trim();
+        String num2 = eTxtB.getText().toString().trim();
+
+        int a = Integer.parseInt(num1);
+        int b = Integer.parseInt(num2);
+
+        int c = a+b;
+
+        txtTitle.setText("sum is: "+c);
+
+        Toast.makeText(this,"You Clicked Button and c is: "+c,Toast.LENGTH_LONG).show();
     }
 
     @Override
