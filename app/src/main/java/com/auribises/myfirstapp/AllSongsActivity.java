74df13f238168de42c7f7078d16c43ef441d6753
Ishant Kumar <er.ishant@gmail.com>
2018-01-12 16:38:05 +0530

    Music Files in a ListView

--- app/src/main/java/com/auribises/myfirstapp/AllSongsActivity.java
+++ app/src/main/java/com/auribises/myfirstapp/AllSongsActivity.java
@@ -1,13 +1,69 @@
 package com.auribises.myfirstapp;
 
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class AllSongsActivity extends AppCompatActivity {
+import java.io.File;
+
+public class AllSongsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
+    void readFilesFromSDCard(){
+
+        listView = findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
+        try {
+
+            String path = Environment.getDownloadCacheDirectory().getAbsolutePath();
+            Log.i("PATH",path);
+            File file = new File(path);
+
+            String[] files = file.list();
+
+            for(String s : files){
+                if(s.endsWith(".mp3")){
+                    adapter.add(s);
+                }
+            }
+
+
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_songs);
+
+        readFilesFromSDCard();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        String name = adapter.getItem(position);
+        Toast.makeText(this,"You Wish to Play: "+name,Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(AllSongsActivity.this,PlaySongActivity.class);
+        intent.putExtra("keySong",name);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/auribises/myfirstapp/PlaySongActivity.java
@@ -0,0 +1,13 @@
+package com.auribises.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaySongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+    }
+}
